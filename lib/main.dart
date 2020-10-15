faaf394845bebbb7a2ab40a3c47de05706a6af72
Aziz Amerul Faozi <admin@labseni.com>
2020-10-15 23:16:27 +0700

    update some code you must read it

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,70 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 void main(){
   runApp(MyApp());
+
 }
 
-class MyApp extends  StatelessWidget{
+//void main()=>runApp(MyApp());
+
+
+class MyApp extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState(){
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion(){
+   setState((){
+        _questionIndex=_questionIndex+1;
+    });
+   // questionIndex=questionIndex+1;
+    print('Answer choosen !');
+  }
+
+  /* standard banget lah di flutter */
+  @override
   Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello !'), );
+    
+    const questions= [
+      {'questionText': 'What\s your favorite color?', 'answers':[
+          'Black', 'Red', 'Green', 'White'],
+      },
+      {'questionText': 'What\s your favorite animal?', 'answers':[
+          'Rabbit', 'Snake', 'Elephant', 'Lizard'],
+      },
+      {'questionText': 'What\s your favorite instructor?', 'answers':[
+          'Max', 'Max', 'Max', 'Max'],
+      },
+      
+    ];
+    
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hanisa App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(
+              questions[_questionIndex]['questionText']
+            ),
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
+                return Answer(_answerQuestion, answer);
+            }).toList()
+            
+          ],
+        ),
+      ),
+    );
   }
   
 }
