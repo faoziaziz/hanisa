326ac1f78e47436e5e942a5f4e59dd20376d1ad2
Aziz Amerul Faozi <admin@labseni.com>
2020-10-16 14:38:52 +0700

    dada

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+//import './question.dart';
+//import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main(){
   runApp(MyApp());
@@ -21,50 +23,83 @@ class MyApp extends StatefulWidget{
 
 
 class _MyAppState extends State<MyApp> {
+  final _questions= const [
+    {
+      'questionText': 'What\s your favorite color?',
+      'answers':[
+        {'text': 'Black', 'score': 10},
+        {'text':'Red', 'score': 5},
+        {'text':'Green', 'score': 3},
+        {'text':'White', 'score':1},
+      ],
+    },
+    {
+      'questionText': 'What\s your favorite animal?',
+      'answers':[
+        {'text':'Rabbit', 'score': 3},
+        {'text':'Snake', 'score': 11},
+        {'text':'Elephant', 'score': 5},
+        {'text':'Lion', 'score': 9},
+      ],
+    },
+    {'questionText': 'What\s your favorite instructor?',
+      'answers':[
+        {'text':'Max', 'score':1},
+        {'text':'Max', 'score':1},
+        {'text':'Max', 'score':1},
+        {'text':'Max', 'score':1},
+      ],
+    },
+    
+  ];
+
   var _questionIndex = 0;
-  void _answerQuestion(){
-   setState((){
+  var _totalScore=0;
+
+  void _resetQuiz(){
+    setState((){
+        _questionIndex=0;
+        _totalScore=0;
+    });
+  }
+  void _answerQuestion(int score){
+    // var aBool=true
+    // aBool = false
+    _totalScore +=score;
+    
+    setState((){
         _questionIndex=_questionIndex+1;
     });
-   // questionIndex=questionIndex+1;
-    print('Answer choosen !');
+
+    
+    if(_questionIndex<_questions.length){
+      print("we have more question !");
+    } else {
+      print("No more question.");
+      
+      // questionIndex=questionIndex+1;
+      //print('Answer choosen !');
+    }
   }
 
   /* standard banget lah di flutter */
   @override
   Widget build(BuildContext context){
     
-    const questions= [
-      {'questionText': 'What\s your favorite color?', 'answers':[
-          'Black', 'Red', 'Green', 'White'],
-      },
-      {'questionText': 'What\s your favorite animal?', 'answers':[
-          'Rabbit', 'Snake', 'Elephant', 'Lizard'],
-      },
-      {'questionText': 'What\s your favorite instructor?', 'answers':[
-          'Max', 'Max', 'Max', 'Max'],
-      },
-      
-    ];
-    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Hanisa App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(
-              questions[_questionIndex]['questionText']
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
-                return Answer(_answerQuestion, answer);
-            }).toList()
-            
-          ],
-        ),
+        body: _questionIndex<_questions.length
+        ? Quiz(
+          answerQuestion:_answerQuestion,
+          questionIndex: _questionIndex,
+          questions: _questions,
+        ): Result(_totalScore, _resetQuiz),
       ),
     );
   }
   
 }
+
