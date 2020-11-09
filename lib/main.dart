dec4b807e35dc5a4e7c7361483b3733460b7ed03
Aziz Amerul Faozi <admin@labseni.com>
2020-11-09 19:59:36 +0700

    add for date intl pacakge

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@
 
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
+
 void main(){
   runApp(
     MyApp());}
@@ -23,7 +25,7 @@ class MyHomePage extends StatelessWidget {
   final List <Transaction> transactions= [
     Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now(),
     ),
-    Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now(),),
+    Transaction(id: 't2', title: 'old Shoes', amount: 212.9, date: DateTime.now(),),
   ];
   @override
   Widget build(BuildContext context){
@@ -53,10 +55,42 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(children: transactions.map((tx){
                 return Card(
-                  child: Text(tx.title),
-                );
-            }).toList(),),
-        ],
+                  child: Row(children: <Widget> [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10), 
+                        child:
+                        Text(
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: <Widget>[
+                          Text(tx.title,
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,),
+                          ),
+                          Text(DateFormat('yyyy-MM-dd').format(tx.date),
+                            style: TextStyle(color: Colors.grey,),
+                          ), 
+                      ],),
+                ],),);
+            }).toList(),),],
       ),
     );
   }
